b97542f58642f239a06267b7f8f4ed837ceb9c44
AgyLuczak <aaluczak1@gmail.com>
2024-03-12 19:55:25 +0000

    Enhance sorting funcionality with ascending and descending order

--- products/views.py
+++ products/views.py
@@ -4,65 +4,66 @@ from django.db.models import Q
 from django.db.models.functions import Lower
 from .models import Product, Category  # Correct import for your models
 
-# Your view functions follow here...
-
-
-
-
 def all_products(request):
-    """
-    A view to show all products, including sorting and search queries.
-    """
     products = Product.objects.all()
     current_categories = Category.objects.all()
     query = None
     categories = None
-    sort = None
-    direction = "asc"
+    sortkey = request.GET.get('sort', None)
+    direction = request.GET.get('direction', 'asc')
 
-    if request.GET:
-        if 'sort' in request.GET:
-            sortkey = request.GET['sort']
-            sort = sortkey
-            if sortkey == 'name':
-                sortkey = 'lower_name'
-                products = products.annotate(lower_name=Lower('name'))
-            elif sortkey == 'category':
-                sortkey = 'category__name'
-            if 'direction' in request.GET:
-                direction = request.GET['direction']
-            sortkey = f'-{sortkey}' if direction == 'desc' else sortkey
-            products = products.order_by(sortkey)
+    # Initialize flags
+    is_sorting_default = True
+    is_price_asc = is_price_desc = is_name_asc = is_name_desc = is_category_asc = is_category_desc = False
 
-        if 'category' in request.GET:
-            categories = request.GET['category'].split(',')
-            products = products.filter(category__name__in=categories)
-            current_categories = Category.objects.filter(name__in=categories)
+    if sortkey and direction:
+        if sortkey == 'name':
+            sortkey = 'lower_name'
+            products = products.annotate(lower_name=Lower('name'))
+        elif sortkey == 'category':
+            sortkey = 'category__name'
 
-        if 'q' in request.GET:
-            query = request.GET['q']
-            if not query:
-                messages.error(request, "You didn't enter any search criteria!")
-                return redirect(reverse('products'))
-            
-            queries = Q(name__icontains=query) | Q(description__icontains=query)
-            products = products.filter(queries)
+        # Apply sorting
+        if direction == 'desc':
+            sortkey = f'-{sortkey}'
+        products = products.order_by(sortkey)
 
-    current_sorting = f'{sort}_{direction}' if sort and direction else 'None_None'
+        # Update flags based on current_sorting
+        current_sorting = f'{sortkey}_{direction}'
+        is_sorting_default = False
+        is_price_asc = current_sorting == 'price_asc'
+        is_price_desc = current_sorting == 'price_desc'
+        is_name_asc = current_sorting == 'name_asc'
+        is_name_desc = current_sorting == 'name_desc'
+        is_category_asc = current_sorting == 'category__name_asc'
+        is_category_desc = current_sorting == 'category__name_desc'
+
+    if 'category' in request.GET:
+        categories = request.GET['category'].split(',')
+        products = products.filter(category__name__in=categories)
+        current_categories = Category.objects.filter(name__in=categories)
+
+    if 'q' in request.GET:
+        query = request.GET['q']
+        if not query:
+            messages.error(request, "You didn't enter any search criteria!")
+            return redirect(reverse('products'))
+        
+        queries = Q(name__icontains=query) | Q(description__icontains=query)
+        products = products.filter(queries)
 
     context = {
         'products': products,
         'search_term': query,
         'current_categories': current_categories,
-        'current_sorting': current_sorting,
-        'is_sorting_default': current_sorting == 'None_None',
-        'is_price_asc': current_sorting == 'price_asc',
-        'is_price_desc': current_sorting == 'price_desc',
-        'is_name_asc': current_sorting == 'name_asc',
-        'is_name_desc': current_sorting == 'name_desc',
-        'is_category_asc': current_sorting == 'category_asc',
-        'is_category_desc': current_sorting == 'category_desc',
-        'border_class': 'info' if current_sorting == 'None_None' else 'black',
+        'is_sorting_default': is_sorting_default,
+        'is_price_asc': is_price_asc,
+        'is_price_desc': is_price_desc,
+        'is_name_asc': is_name_asc,
+        'is_name_desc': is_name_desc,
+        'is_category_asc': is_category_asc,
+        'is_category_desc': is_category_desc,
+        'border_class': 'info' if is_sorting_default else 'black',
     }
 
     return render(request, 'products/products.html', context)
@@ -78,6 +79,3 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
-
-
-
