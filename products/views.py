d5091c028f149572f087963a670a9794ad925c2a
AgyLuczak <aaluczak1@gmail.com>
2024-03-21 21:01:22 +0000

    Product admin - finish add product functionality

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
+from django.urls import reverse
 
 from .models import Product, Category  # Correct import for your models
 from .forms import ProductForm
@@ -85,7 +86,17 @@ def product_detail(request, product_id):
 
 def add_product(request):
     """ Add a product to the store """
-    form = ProductForm()
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ProductForm()
+        
     template = 'products/add_product.html'
     context = {
         'form': form,
--- profiles/views.py
+++ profiles/views.py
@@ -16,8 +16,10 @@ def profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully')
-
-    form = UserProfileForm(instance=profile)
+        else:
+            messages.error(request, 'Update failed. Please ensure the form is valid.')
+    else:
+        form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
@@ -25,8 +27,9 @@ def profile(request):
         'form': form,
         'orders': orders,
         'on_profile_page': True
-
     }
+
+    return render(request, template, context)
 
     return render(request, template, context)
 
