b911f12fad64c8d67203a41f2eb4afadcfe5fcfd
AgyLuczak <aaluczak1@gmail.com>
2024-04-10 04:31:40 +0000

    Fix NameError by importing reverse in views

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse 
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import WishlistItem
