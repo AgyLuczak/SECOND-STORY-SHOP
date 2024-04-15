1e738785f6ed9eecd19e27cd8fa668127f040f6c
AgyLuczak <aaluczak1@gmail.com>
2024-04-15 21:26:01 +0000

    Remove incorrect import line

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,39 +1,27 @@
 from django import forms
 from .models import UserProfile
-from .forms import UserProfileForm
 from django.utils.translation import gettext as _
-
-
-
 
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         exclude = ('user',)
 
-    # Add the __init__ methods to set custom classes to form fields
     def __init__(self, *args, **kwargs):
         super(UserProfileForm, self).__init__(*args, **kwargs)
-        for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'form-field form-field-focus'
-
-        super().__init__(*args, **kwargs)
         placeholders = {
-            'default_phone_number': 'Phone Number',
-            'default_postcode': 'Postal Code',
-            'default_town_or_city': 'Town or City',
-            'default_street_address1': 'Street Address 1',
-            'default_street_address2': 'Street Address 2',
-            'default_county': 'County, State or Locality',
+            'default_phone_number': _('Phone Number'),
+            'default_postcode': _('Postal Code'),
+            'default_town_or_city': _('Town or City'),
+            'default_street_address1': _('Street Address 1'),
+            'default_street_address2': _('Street Address 2'),
+            'default_county': _('County, State or Locality'),
         }
 
         self.fields['default_phone_number'].widget.attrs['autofocus'] = True
         for field in self.fields:
             if field != 'default_country':
-                if self.fields[field].required:
-                    placeholder = f'{placeholders[field]} *'
-                else:
-                    placeholder = placeholders[field]
-                self.fields[field].widget.attrs['placeholder'] = placeholder
-            self.fields[field].widget.attrs['class'] = 'border-black rounded-0 profile-form-input'
-            self.fields[field].label = False
+                placeholder = placeholders.get(field, '')
+                self.fields[field].widget.attrs['placeholder'] = f"{placeholder} {'*' if self.fields[field].required else ''}"
+                self.fields[field].widget.attrs['class'] = 'border-black rounded-0 profile-form-input focus-highlight'  # Added focus-highlight class
+                self.fields[field].label = False
