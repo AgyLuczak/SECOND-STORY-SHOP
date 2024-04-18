7ccd8f036d78c659ca7b36a10b1f028d58c56d97
AgyLuczak <aaluczak1@gmail.com>
2024-04-18 21:15:11 +0000

    Fix the missing buttons class

--- profiles/forms.py
+++ profiles/forms.py
@@ -18,10 +18,16 @@ class UserProfileForm(forms.ModelForm):
             'default_county': _('County, State or Locality'),
         }
 
+        # Set autofocus on the first field
         self.fields['default_phone_number'].widget.attrs['autofocus'] = True
+
+        # Apply placeholder, class, and label settings to all fields
         for field in self.fields:
-            if field != 'default_country':
-                placeholder = placeholders.get(field, '')
-                self.fields[field].widget.attrs['placeholder'] = f"{placeholder} {'*' if self.fields[field].required else ''}"
-                self.fields[field].widget.attrs['class'] = 'stripe-form-input focus-highlight'
-                self.fields[field].label = False
+            placeholder = placeholders.get(field, self.fields[field].help_text)
+            self.fields[field].widget.attrs['placeholder'] = f"{placeholder} {'*' if self.fields[field].required else ''}"
+            self.fields[field].widget.attrs['class'] = 'profile-form-input focus-highlight'
+            self.fields[field].label = False
+
+        # Handle specific field if needed (e.g., a dropdown for country)
+        if 'default_country' in self.fields:
+            self.fields['default_country'].widget.attrs['class'] = 'profile-form-input select-focus-highlight'
