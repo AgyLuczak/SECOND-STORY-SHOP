adb5ff424b443fc865a321f8f2235bef620fb7c5
AgyLuczak <aaluczak1@gmail.com>
2024-04-25 13:25:01 +0000

    Disable wishlist toast in base.html

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.urls import reverse 
 from django.contrib.auth.decorators import login_required
