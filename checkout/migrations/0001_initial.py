fbedb9ba9a6324cea5adedb129acdf6e9200b20f
AgyLuczak <aaluczak1@gmail.com>
2024-05-01 10:52:36 +0000

    Change size to Integer in all models

--- checkout/migrations/0001_initial.py
+++ checkout/migrations/0001_initial.py
@@ -37,7 +37,8 @@ class Migration(migrations.Migration):
             name='OrderLineItem',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('product_size', models.CharField(blank=True, max_length=2, null=True)),
+                ('product_size', models.IntegerField(blank=True, null=True)),
+                # ('product_size', models.CharField(max_length=2, null=True, blank=True)), # XS, S, M, L, XL
                 ('quantity', models.IntegerField(default=0)),
                 ('lineitem_total', models.DecimalField(decimal_places=2, editable=False, max_digits=6)),
                 ('order', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='lineitems', to='checkout.order')),
--- checkout/models.py
+++ checkout/models.py
@@ -89,7 +89,8 @@ class OrderLineItem(models.Model):
         Product, null=False, blank=False, on_delete=models.CASCADE
     )
     product_size = models.IntegerField(
-    null=True, blank=True) 
+    null=True, blank=True)
+    # product_size = models.CharField(max_length=2, null=True, blank=True) # XS, S, M, L, XL
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(
         max_digits=6, decimal_places=2, null=False, blank=False, editable=False
