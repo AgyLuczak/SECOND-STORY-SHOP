94757ee616aa116d55fecf4dc87738387a4d1582
AgyLuczak <aaluczak1@gmail.com>
2024-05-07 20:31:59 +0000

    Set debug to false

--- second_story/settings.py
+++ second_story/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = False
 # DEBUG = os.environ.get('DEVELOPMENT') == 'True'
 
 ALLOWED_HOSTS = ALLOWED_HOSTS = [
